d538fe9a3d98fec92757f0b87a8428797c9d24eb
Hồ Kim Thành <hokimthanh8@gmail.com>
2026-02-05 16:19:25 +0700

    a

--- api/levels/[id].js
+++ api/levels/[id].js
@@ -42,7 +42,6 @@ export default async function handler(req, res) {
   res.status(200).json({
     levelId: levelId,
     name: levelData.name,
-    difficulty: levelData.difficulty,
     hasUpdate: true,
     downloadUrl: levelData.url,
     fileSize: levelData.fileSize,
